3a6fc264e3d5921585cf56e81de3daf4794b76a1
BARATHM111 <barathm111@gmail.com>
2026-02-06 04:42:29 +0530

    Auto push: 2026-02-06 04:42:29

--- basics.cpp
+++ basics.cpp
@@ -38,7 +38,7 @@ void Printpattern3(){
     int n;
     cin>>n;
     for (int i=0; i<n; i++){
-        for (int j=1; j<=i; j++){
+        for (int j=1; j<i; j++){
             cout<<j<<' ';
         }
         cout<<endl;
