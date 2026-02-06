63e92743223a6e70c35bb84ac481cf2f3a2cdece
BARATHM111 <barathm111@gmail.com>
2026-02-06 06:09:07 +0530

    Auto push: 2026-02-06 06:09:07

--- basics.cpp
+++ basics.cpp
@@ -1,131 +1,126 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-void Printpattern1(){
-    int n ;
+void Printpattern1()
+{
+    int n;
     cin >> n;
 
-    for (int i=0; i< n; i++){
-        for (int j=0; j<n; j++){
-            cout<<'x'<<" ";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << 'x' << " ";
         }
 
-    
-        cout<<endl;
+        cout << endl;
     }
-    
-    
-    
-
-
 }
-void Printpattern2(){
-    int n ;
-    cin >>n;
-    for (int i=0; i<n; i++){
-        for (int j=0; j<=i; j++){
-            cout<<"x"<<" ";
-
-        }
-        cout<<endl;
-
-    }
-
-
-}
-void Printpattern3(){
+void Printpattern2()
+{
     int n;
-    cin>>n;
-    for (int i=0; i<n; i++){
-        // spaces 
-        for (int j=0; j<n-i-1; j++){
-            cout<<' ';
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "x" << " ";
         }
-        // star
-        for (int j=0; j<2*i+1; j++){
-            cout<<'x';
-        }
-        // spaces 
-        for (int j=0; j<n-i-i; j++){
-            cout<<' ';
-        }
-        cout<<endl;
+        cout << endl;
     }
-
-
-
 }
-void Printpattern4(){
+void Printpattern3()
+{
     int n;
-    cin>>n;
-
-
-    for (int i=0; i<n; i++){
-        // spaces 
-        for (int j=0; j<n-i-1; j++){
-            cout<<' ';
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        // spaces
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << ' ';
         }
         // star
-        for (int j=0; j<2*i+1; j++){
-            cout<<'x';
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << 'x';
         }
-        // spaces 
-        for (int j=0; j<n-i-i; j++){
-            cout<<' ';
+        // spaces
+        for (int j = 0; j < n - i - i; j++)
+        {
+            cout << ' ';
         }
-        cout<<endl;
+        cout << endl;
     }
-
-
-    for (int i=0; i<n; i++){
-        // spaces 
-        for (int j=0; j<i; j++){
-            cout<<' ';
-        }
-        // star
-        for (int j=0; j<2*n-(2*i+1); j++){
-            cout<<'x';
-        }
-        // spaces 
-        for (int j=0; j<i; j++){
-            cout<<' ';
-        }
-        cout<<endl;
-    }
-
-
-
 }
-void Printpattern5(int n){
-    
+void Printpattern4()
+{
+    int n;
+    cin >> n;
 
-    for (int i=0; i<(2*n-1); i++){
+    for (int i = 0; i < n; i++)
+    {
+        // spaces
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << ' ';
+        }
+        // star
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << 'x';
+        }
+        // spaces
+        for (int j = 0; j < n - i - i; j++)
+        {
+            cout << ' ';
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        // spaces
+        for (int j = 0; j < i; j++)
+        {
+            cout << ' ';
+        }
+        // star
+        for (int j = 0; j < 2 * n - (2 * i + 1); j++)
+        {
+            cout << 'x';
+        }
+        // spaces
+        for (int j = 0; j < i; j++)
+        {
+            cout << ' ';
+        }
+        cout << endl;
+    }
+}
+void Printpattern5(int n)
+{
+
+    for (int i = 0; i < (2 * n - 1); i++)
+    {
         int stars = 1;
-        if(i>=n)
-            stars = 2*n - i;
-        else 
-        stars = i+1;
-        
-        for (int j=1; j<=stars; j++){
-            cout<<'x';
+        if (i >= n)
+            stars = 2 * n - i;
+        else
+            stars = i + 1;
+
+        for (int j = 1; j <= stars; j++)
+        {
+            cout << 'x';
         }
-        
-        cout<<endl;
+
+        cout << endl;
     }
-
-
-
 }
-int main(){
+int main()
+{
     int n;
     cin >> n;
 
-  
-  Printpattern5(n);
-
-
-
-
+    Printpattern5(n);
 }
-
