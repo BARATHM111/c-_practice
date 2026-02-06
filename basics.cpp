99eff6f2fe76a98804b2b9129c61d57e67a63248
BARATHM111 <barathm111@gmail.com>
2026-02-06 05:53:02 +0530

    Auto push: 2026-02-06 05:53:02

--- basics.cpp
+++ basics.cpp
@@ -97,11 +97,29 @@ void Printpattern4(){
 
 
 }
+void Printpattern5(int n){
+ 
 
+    
+    for (int i=0; i<(2*n-1); i++){
+        int stars = 1;
+        if(i>n){
+            stars = 2*n - i;
+        }
+        for (int j=1; j<=stars; j++){
+            cout<<'x';
+        }
+        
+        cout<<endl;
+    }
+
+
+
+}
 int main(){
 
-  Printpattern3();
-  Printpattern4();
+  
+  Printpattern5(5);
 
 
 
